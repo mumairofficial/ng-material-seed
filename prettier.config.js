6d879a940457b95b25602d836b29104abbb972b3
Muhammad Umair <mumairofficial@gmail.com>
2020-03-25 20:49:19 +0500

    config: static analysers and prettier formatting

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 140,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+};
