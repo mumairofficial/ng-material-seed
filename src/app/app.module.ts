07e8ca76303574e4d0ee52d4f62c71b90fab0a3d
Muhammad Umair <mumairofficial@gmail.com>
2020-03-28 12:08:54 +0500

    chore(temporary): add common module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { CoreModule } from '@app-core/core.module';
 
@@ -7,7 +8,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule, AppRoutingModule],
+  imports: [CoreModule, CommonModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
